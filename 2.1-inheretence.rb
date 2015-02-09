8fd78ccec242e1e0f1381fb96ef5cb79f1e9f6b8
Toth Peter <tothpeter08@gmail.com>
2015-02-09 22:41:35 +0100

    2.1 Inheritance

---
+++ 2.1-inheretence.rb
@@ -0,0 +1,27 @@
+class Person
+  attr_reader :first_name, :last_name
+
+  def initialize first_name, last_name, gender
+    @first_name = first_name
+    @last_name = last_name
+    @gender = gender
+  end
+
+  def full_name
+    first_name + " " + last_name
+  end
+end
+
+class Screencaster < Person
+  def initialize first_name, last_name, gender, tools
+    super first_name, last_name, gender
+    @tools = tools
+  end
+end
+
+class Student < Person
+  def initialize first_name, last_name, gender, preferred_language
+    super first_name, last_name, gender
+    @preferred_language = preferred_language
+  end
+end
