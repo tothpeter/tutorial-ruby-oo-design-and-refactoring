1b3bde4100c8368e8749ee0ee14ae061e34bb8be
Toth Peter <tothpeter08@gmail.com>
2015-02-12 19:40:13 +0100

    3.3-Pull Up Method

---
+++ 3.3_pull_up_method/test.rb
@@ -0,0 +1,17 @@
+require "minitest/spec"
+require "minitest/autorun"
+
+require "./before" if ENV["BEFORE"]
+require "./after" unless ENV["BEFORE"]
+
+describe MalePerson do
+  it "has a full name" do
+    MalePerson.new("John", "Smith").full_name.must_equal "John Smith"
+  end
+end
+
+describe FemalePerson do
+  it "has a full name" do
+    MalePerson.new("Michelle", "Smith").full_name.must_equal "Michelle Smith"
+  end
+end
