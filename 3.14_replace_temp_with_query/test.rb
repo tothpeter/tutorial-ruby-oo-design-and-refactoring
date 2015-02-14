ff133b71d6cb77c51da8701596df09230d002b76
Toth Peter <tothpeter08@gmail.com>
2015-02-14 15:07:34 +0100

    3.14 Replace Temp with Query

---
+++ 3.14_replace_temp_with_query/test.rb
@@ -0,0 +1,11 @@
+require "minitest/spec"
+require "minitest/autorun"
+
+require "./before" if ENV["BEFORE"]
+require "./after" unless ENV["BEFORE"]
+
+describe Cuboid do
+  it "has a volume" do
+    Cuboid.new(2,3,4).volume.must_equal 24
+  end
+end
