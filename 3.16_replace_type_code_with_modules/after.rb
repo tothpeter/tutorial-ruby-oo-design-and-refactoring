eea788fce6a1cf18f78c26f523139f47159c5092
Toth Peter <tothpeter08@gmail.com>
2015-02-14 15:44:16 +0100

    3.16-Replace Type Code with Modules

---
+++ 3.16_replace_type_code_with_modules/after.rb
@@ -0,0 +1,33 @@
+class Employee
+
+  def base_salary
+    500.0
+  end
+
+  def salary
+    base_salary + bonus
+  end
+
+  def self.build type: :regular
+    instance = new
+    instance.extend const_get(type.capitalize)
+  end
+end
+
+module Regular
+  def bonus
+    0
+  end
+end
+
+module Boss
+  def bonus
+    1500.0
+  end
+end
+
+module Manager
+  def bonus
+    800.0
+  end
+end
